bb8a9bcdff595d0d7119513e7ff889d75d2267ae
JesusPQ15 <105338462+JesusPQ15@users.noreply.github.com>
2024-10-29 10:43:35 -0500

    Tabla Departamento

---
+++ TablaDepartamento/CRUDurtianguisTF/.angular/cache/18.2.10/CRUDurtianguisTF/vite/deps/@angular_material_input.js
@@ -0,0 +1,33 @@
+import {
+  MAT_INPUT_VALUE_ACCESSOR,
+  MatInput,
+  MatInputModule,
+  getMatInputUnsupportedTypeError
+} from "./chunk-XOZUDAH4.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-P3SUMBJZ.js";
+import "./chunk-H4WBRGT6.js";
+import "./chunk-ZZ5HLOIY.js";
+import "./chunk-4SRJJHXW.js";
+import "./chunk-UP3664T6.js";
+import "./chunk-MJ6RAITD.js";
+import "./chunk-PF6LNL77.js";
+export {
+  MAT_INPUT_VALUE_ACCESSOR,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatInput,
+  MatInputModule,
+  MatLabel,
+  MatPrefix,
+  MatSuffix,
+  getMatInputUnsupportedTypeError
+};
+//# sourceMappingURL=@angular_material_input.js.map
