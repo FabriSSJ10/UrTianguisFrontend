0e16e46f3e462abc41076ec6ba85166098f1df86
JesusPQ15 <105338462+JesusPQ15@users.noreply.github.com>
2024-10-29 10:45:39 -0500

    Tabla Departamento

---
+++ TablaDepartamento/CRUDurtianguisTF/.angular/cache/18.2.10/CRUDurtianguisTF/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,26 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-FMX7AXUA.js";
+import "./chunk-NBDRSUDG.js";
+import "./chunk-NLAQVFGP.js";
+import "./chunk-UJW43TT6.js";
+import "./chunk-DQZ2PILJ.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-TKQWSO4T.js";
+import "./chunk-2H3L6IVL.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
